40d172e04d673ad09c1ffc2f472dcbbec5d74918
Miraç DAĞ <dagella.mrc@gmail.com>
2024-12-01 23:39:58 +0300

    opp details

---
+++ app/src/main/java/com/minadag/javaoopdetails/Cat.java
@@ -0,0 +1,31 @@
+package com.minadag.javaoopdetails;
+
+public class Cat {
+
+    /* private yapmazsak mainActivity de ulasim saglarız
+
+    private String name;
+    private String eyesColor;
+    private int years;
+     */
+
+
+    String name;
+    String eyesColor;
+    int years;
+
+
+    public Cat (String name, String eyesColor, int years ){
+
+        this.name = name;
+        this.eyesColor = eyesColor;
+        this.years = years;
+
+    }
+
+    public  Cat (){
+
+    }
+
+}
+
